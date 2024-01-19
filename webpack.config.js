513bc84f0958b5aed1e8c734d1871a9b430974c2
박수정 <sjj083015@gmail.com>
2024-01-19 16:18:07 +0900

    [MOD] 오타 수정 (build시 오류 발생 수정)

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                user: [
+                use: [
                     'babel-loader'
                 ]
             }
